0f69bc46f100592ac4af12d80db4e01993d61ec1
sollimann <kristoffer.solberg@cognite.com>
2020-11-28 23:01:22 +0100

    add request and server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
 fn main() {
-    println!("Hello, world!");
+    let get = Method::GET("abcd".to_string());
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+// Struct definition
+struct Server {
+    addr: String,
+}
+
+// Struct implementation
+impl Server {
+    // associated function
+    // same as a staticmethod in Python
+    fn new(addr: String) -> Self {
+        // 'Self' is alias for 'Server'
+        Self {
+            addr
+        }
+    }
+
+    // method
+    // always takes a 'self'
+    fn run(self) {
+
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: String
+}
+
+enum Method {
+    GET(String),
+    DELETE(u64),
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }
