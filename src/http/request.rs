e01cf22822f1d3cc014e64fac87b3f4ae50d0455
sollimann <kristoffer.solberg@cognite.com>
2020-11-29 20:26:11 +0100

    more parsing of request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{MethodError, Method};
 use std::str;
 use std::str::Utf8Error;
 use std::convert::TryFrom;
@@ -21,12 +21,21 @@ impl TryFrom<&[u8]> for Request {
 
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol)
         }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i+1..]);
+            path = &path[..i];
+        }
+
         unimplemented!();
     }
 }
@@ -60,6 +69,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method"
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_:MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
