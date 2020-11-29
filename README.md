dee5ebb3c7ff0d771a7707b065731360cf66df34
sollimann <kristoffer.solberg@cognite.com>
2020-11-29 16:57:48 +0100

    http server running on port 8080

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # http-server
+
+To test server, run the application:
+
+```bash
+$ cargo run
+```
+
+and pipe a request "TEST" to the TCP socket (IP address + Port number = web socket) 
+```bash
+$ echo "TEST" | netcat 127.0.0.1 8080
+```
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 // Struct definition
 pub struct Server {
@@ -17,9 +18,27 @@ impl Server {
     // always takes a 'self'
     pub fn run(self) {
         println!("Listening on {}", self.addr);
-
+        let listener = TcpListener::bind(&self.addr).unwrap();
         loop {
-            let listener = TcpListener::bind(&self.addr).unwrap();
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024]; // array of 0 and of 1024 bytes
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                        println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e)
+                    }
+                    println!("OK");
+                },
+                Err(e) => println!("Failed to estabish a connection: {}", e)
+            }
+
+            let res = listener.accept();
+            if res.is_err(){
+                continue;
+            }
+            let (stream, addr) = res.unwrap();
         }
     }
 }
