2c8affeaa353b59520cb9290554261816c497760
sollimann <kristoffer.solberg@cognite.com>
2020-11-29 19:54:31 +0100

    added method for parsing http request into METHOD-PATH-PROTOCOL

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,6 @@
 use super::method::Method;
+use std::str;
+use std::str::Utf8Error;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Debug};
@@ -13,10 +15,32 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
+    // GET /search?name=abc&sort=1 HTTP/1.1 (METHOD /Path Protocol)
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol)
+        }
         unimplemented!();
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    // returns an iterator over the [char]s of a string slice
+    let mut iter = request.chars();
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[(i+1)..]))
+        }
+    }
+
+    None
 }
 
 // Our custom error type
@@ -38,6 +62,13 @@ impl ParseError {
         }
     }
 }
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 // implement the error trait (interface) from std library
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
