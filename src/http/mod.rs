b6c9d43bcf97103be403571772e379388575f7e7
sollimann <kristoffer.solberg@cognite.com>
2020-11-30 21:38:09 +0100

    added status code

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,8 +3,12 @@ pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
+pub use status_code::StatusCode;
 
 // make http folder visible for import
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
+pub mod status_code;
